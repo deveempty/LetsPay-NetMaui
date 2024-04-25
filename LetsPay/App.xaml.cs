d6e03cfeb0aa2036b65cd7aa8e1648d4d2f8dce0
Juan Viloria <juanviloriav@gmail.com>
2024-04-25 13:48:58 -0500

    LetsPay init commit

---
+++ LetsPay/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LetsPay;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ LetsPay/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LetsPay;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
